2c3f7e889fc862233d1f81ff531cb40448ce0484
Borislav Borisov <briffraff6@gmail.com>
2025-07-12 03:09:33 +0300

    UI to external file

--- lan_burboran.py
+++ lan_burboran.py
@@ -1,49 +1,30 @@
 import sys
+from UI.lan_burboran_UI import LanBurboran_UI
 
-from PySide6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
-                               QTextEdit, QListWidget, QLineEdit, QPushButton,
-                               QLabel, QMessageBox)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox)
 
 
-class LanBurboran(QWidget):
+class LanBurboran:
     def __init__(self, is_server=False):
-        super().__init__()
+        self.window = QMainWindow()
+        self.window_ui = LanBurboran_UI()
+        self.window_ui.setupUi(self.window)
+
         self.is_server = is_server
-        self.init_ui()
-
-    def init_ui(self):
-        self.setWindowTitle("LAN Burboran")
-        layout = QVBoxLayout()
-        header = QLabel(f"Стая: Седянка • Онлайн: 1")
-        self.header = header
-        layout.addWidget(header)
-
-        main_layout = QHBoxLayout()
-        self.user_list = QListWidget()
-        self.chat_view = QTextEdit(readOnly=True)
-        main_layout.addWidget(self.user_list, 1)
-        main_layout.addWidget(self.chat_view, 3)
-        layout.addLayout(main_layout)
-
-        bottom = QHBoxLayout()
-        self.input = QLineEdit()
-        send_btn = QPushButton("Изпрати")
-        bottom.addWidget(self.input)
-        bottom.addWidget(send_btn)
-        layout.addLayout(bottom)
-
-        self.setLayout(layout)
-        self.show()
 
 
 def main():
     app = QApplication(sys.argv)
     reply = QMessageBox.question(None, "Избор", "Създаване на стая (Server)?",
                                  QMessageBox.Yes | QMessageBox.No)
-    is_server = (reply == QMessageBox.Yes)
-    w = LanBurboran(is_server)
 
-    sys.exit(app.exec())
+    is_server = (reply == QMessageBox.Yes)
+
+    lanburboran = LanBurboran(is_server)
+    lanburboran_window = lanburboran.window
+    lanburboran_window.show()
+
+    app.exec()
 
 
 if __name__ == "__main__":
